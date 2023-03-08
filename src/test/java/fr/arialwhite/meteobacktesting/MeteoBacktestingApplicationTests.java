2d4e3513fe427cb2da0ee1b69e7ad0cfec0cc342
Arialwhite <contact@julianguillotel.fr>
2023-03-08 21:47:18 +0100

    initial commit

---
+++ src/test/java/fr/arialwhite/meteobacktesting/MeteoBacktestingApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.arialwhite.meteobacktesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeteoBacktestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
